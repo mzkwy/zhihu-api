29ecbd18573d3233c3c6e6ea97c15cf68a0c4341
syaning <syaningv@gmail.com>
2016-03-13 23:31:38 +0800

    test config

---
+++ test/config.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect
+const config = require('../lib/config')
+
+describe('config', function() {
+    it('config.headers', function() {
+        expect(config.headers).to.have.property('User-Agent')
+            .which.is.a('string')
+
+        expect(config.headers).to.have.property('Cookie')
+            .which.is.a('string')
+    })
+
+    it('config.setCookie(cookie)', function() {
+        var cookie = 'this is my cookie'
+        expect(config.setCookie).to.be.a('function')
+        config.setCookie(cookie)
+        expect(config.headers.Cookie).to.equal(cookie)
+    })
+
+    it('config.setUserAgent(userAgent)', function() {
+        var userAgent = 'this is my user-agent'
+        expect(config.setUserAgent).to.be.a('function')
+        config.setUserAgent(userAgent)
+        expect(config.headers['User-Agent']).to.equal(userAgent)
+    })
+})
