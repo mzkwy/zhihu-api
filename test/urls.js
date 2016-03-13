a9a329398147d5bcd96413b46678b589e989e744
syaning <syaningv@gmail.com>
2016-03-13 23:04:11 +0800

    add test

---
+++ test/urls.js
@@ -0,0 +1,57 @@
+const expect = require('chai').expect
+const urls = require('../lib/urls')
+const baseurl = urls._baseurl
+
+describe('urls', function() {
+    it('urls.index()', function() {
+        expect(urls.index()).to.equal(baseurl)
+    })
+
+    it('urls.user.detail(uname)', function() {
+        expect(urls.user.detail('test')).to
+            .equal(baseurl + '/people/test/about')
+    })
+
+    it('urls.user.profile(uname)', function() {
+        expect(urls.user.profile('test')).to
+            .equal(baseurl + '/node/MemberProfileCardV2?params=%7B%22url_token%22%3A%22test%22%7D')
+    })
+
+    it('urls.user.followers()', function() {
+        expect(urls.user.followers()).to
+            .equal(baseurl + '/node/ProfileFollowersListV2')
+    })
+
+    it('urls.user.followees()', function() {
+        expect(urls.user.followees()).to
+            .equal(baseurl + '/node/ProfileFolloweesListV2')
+    })
+
+    it('urls.user.answers(uname)', function() {
+        expect(urls.user.answers('test')).to
+            .equal(baseurl + '/people/test/answers')
+    })
+
+    it('urls.answer.byVote()', function() {
+        expect(urls.answer.byVote()).to
+            .equal(baseurl + '/node/QuestionAnswerListV2')
+    })
+
+    it('urls.answer.byPage(questionId, page)', function() {
+        expect(urls.answer.byPage(41325553)).to
+            .equal(baseurl + '/question/41325553?sort=created&page=1')
+
+        expect(urls.answer.byPage(41325553, 2)).to
+            .equal(baseurl + '/question/41325553?sort=created&page=2')
+    })
+
+    it('urls.question.byId(questionId)', function() {
+        expect(urls.question.byId(41325553)).to
+            .equal(baseurl + '/question/41325553')
+    })
+
+    it('urls.question.latest()', function() {
+        expect(urls.question.latest()).to
+            .equal(baseurl + '/log/questions')
+    })
+})
