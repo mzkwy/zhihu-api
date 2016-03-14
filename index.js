8fc716f44a948fa8aaaedb44ff7822d877c08ea8
syaning <syaningv@gmail.com>
2016-03-14 19:08:44 +0800

    remove lib/index

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require('./lib')
+const config = require('./lib/config')
+const answer = require('./lib/api/answer')
+const question = require('./lib/api/question')
+const user = require('./lib/api/user')
+
+module.exports = function(cookie) {
+	config.setCookie(cookie)
+
+	return {
+		_config: config,
+		answer,
+		question,
+		user
+	}
+}
