94a62f663946d6580c2774b6daacef891067e982
syaning <syaningv@gmail.com>
2016-03-18 11:08:44 +0800

    support change cookie hotly

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const config = require('./lib/config')
 const question = require('./lib/api/question')
 const user = require('./lib/api/user')
 
-module.exports = function(cookie) {
-	config.setCookie(cookie)
+function cookie(_cookie) {
+    if (_cookie) {
+        config.setCookie(_cookie)
+    } else {
+        return config.headers['Cookie']
+    }
+}
 
-	return {
-		_config: config,
-		question,
-		user
-	}
+module.exports = {
+    _config: config,
+    cookie,
+    question,
+    user
 }
