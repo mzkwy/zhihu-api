3146d1b2cadb9a24a7f02bdcea4b4ee6d9397b99
syaning <syaningv@gmail.com>
2016-08-13 23:29:10 +0800

    update config

--- test/config.js
+++ test/config.js
@@ -1,34 +1,40 @@
-const expect = require('chai').expect
+const assert = require('assert')
 const config = require('../lib/config')
 
-describe('config', function() {
-  it('config.headers', function() {
-    expect(config.headers).to.have.property('User-Agent')
-      .which.is.a('string')
-
-    expect(config.headers).to.have.property('Cookie')
-      .which.is.a('string')
+describe('config', () => {
+  it('config.headers', () => {
+    assert(typeof config.headers['User-Agent'] === 'string')
+    assert(typeof config.headers['Cookie'] === 'string')
+    assert(typeof config.headers['Referer'] === 'string')
   })
 
-  it('config.userAgent', function() {
-    var userAgent = 'this is user agent'
-    expect(config.userAgent).to.equal(config.headers['User-Agent'])
-    config.userAgent = userAgent
-    expect(config.headers['User-Agent']).to.equal(userAgent)
+  it('config.userAgent', () => {
+    var headers = config.headers
+    var ua = 'customized user-agent'
+
+    assert(config.userAgent === headers['User-Agent'])
+    config.userAgent = ua
+    assert(ua === headers['User-Agent'])
   })
 
-  it('config.cookie', function() {
-    var cookie = 'this is cookie'
-    expect(config.cookie).to.equal(config.headers['Cookie'])
+  it('config.cookie', () => {
+    var headers = config.headers
+    var cookie = 'hello=world'
+
+    assert(config.cookie === headers['Cookie'])
     config.cookie = cookie
-    expect(config.headers['Cookie']).to.equal(cookie)
+    assert(cookie === headers['Cookie'])
   })
 
-  it('config.xsrf', function() {
-    var xsrf = 'this is xsrf'
+  it('config.xsrf', () => {
+    var xsrf = '1234567890'
     config.xsrf = xsrf
-    expect(config.xsrf).to.equal(xsrf)
-    config.cookie = 'new cookie'
-    expect(config.xsrf).to.equal('')
+    assert(config.xsrf === xsrf)
+
+    config.cookie = 'hello=world'
+    assert(config.xsrf === '')
+
+    config.cookie = 'hello=world; _xsrf=1234567890'
+    assert(config.xsrf === '1234567890')
   })
 })
