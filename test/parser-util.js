a03b9eae60e1b2763bf3471dafc53050ceaebf65
syaning <syaningv@gmail.com>
2016-11-14 22:27:57 +0800

    update util

--- test/parser-util.js
+++ test/parser-util.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect
 const cheerio = require('cheerio')
-const util = require('../../lib/parser/util')
+const util = require('../lib/parser/util')
+const urls = require('../lib/urls')
 
 var domstr = '<div id="example" class="container" data-name="test" data-id="123456" data-count="0">hello 世界</div>'
 var $ = cheerio.load(domstr, {
@@ -9,28 +10,28 @@ var $ = cheerio.load(domstr, {
 var ele = $('div')
 
 describe('parser util', function() {
-  it('util.getAttr(ele, attr)', function() {
+  it('#getAttr', function() {
     expect(util.getAttr(ele, 'id')).to.equal('example')
     expect(util.getAttr(ele, 'class')).to.equal('container')
   })
 
-  it('util.getText(ele)', function() {
+  it('#getText', function() {
     expect(util.getText(ele)).to.equal('hello 世界')
   })
 
-  it('util.getData(ele, name)', function() {
+  it('#getData', function() {
     expect(util.getData(ele, 'name')).to.equal('test')
     expect(util.getData(ele, 'id')).to.equal(123456)
     expect(util.getData(ele, 'count')).to.equal(0)
     expect(util.getData(ele, 'none')).to.equal('')
   })
 
-  it('util.getHtml(ele)', function() {
+  it('#getHtml', function() {
     expect(util.getHtml($)).to.equal(domstr)
     expect(util.getHtml(ele)).to.equal('hello 世界')
   })
 
-  it('util.toNum(val)', function() {
+  it('#toNum', function() {
     expect(util.toNum(null)).to.equal(0)
     expect(util.toNum(undefined)).to.equal(0)
     expect(util.toNum([])).to.equal(0)
@@ -40,16 +41,33 @@ describe('parser util', function() {
     expect(util.toNum('12345')).to.equal(12345)
   })
 
-  it('util.forEach(arr, fn)', function() {
+  it('#forEach', function() {
     var arr = [1, 2, 3]
     var res = []
     util.forEach(arr, x => res.push(x * x))
     expect(res).to.eql([1, 4, 9])
   })
 
-  it('util.map(arr, fn)', function() {
+  it('#map', function() {
     var arr = [1, 2, 3]
     var res = util.map(arr, x => x * x)
     expect(res).to.eql([1, 4, 9])
+  })
+
+  it('parseSlug', function() {
+    var ret = util.parseSlug('/people/test')
+    expect(ret.type).to.equal('people')
+    expect(ret.slug).to.equal('test')
+    expect(ret.link).to.equal(`${urls.baseurl}/people/test`)
+
+    ret = util.parseSlug('/org/test')
+    expect(ret.type).to.equal('org')
+    expect(ret.slug).to.equal('test')
+    expect(ret.link).to.equal(`${urls.baseurl}/org/test`)
+
+    ret = util.parseSlug('')
+    expect(ret.type).to.equal('anonymous')
+    expect(ret.slug).to.equal('')
+    expect(ret.link).to.equal('')
   })
 })
