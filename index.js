2297ba4520d8a1b741af99ed245f99dc092ca17a
syaning <syaningv@gmail.com>
2016-11-14 16:45:42 +0800

    question api

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // const action = require('./lib/api/action')
 // const answer = require('./lib/api/answer')
 // const org = require('./lib/api/org')
-// const question = require('./lib/api/question')
 const Request = require('./lib/request')
 const User = require('./lib/api/user')
 const Topic = require('./lib/api/topic')
+const Question = require('./lib/api/question')
 const package = require('./package')
-
 
 module.exports = API
 
@@ -18,11 +17,13 @@ function API(cookie) {
   var request = new Request()
   request.setCookie(cookie)
 
+  User.prototype.__proto__ = request
+  Topic.prototype.__proto__ = request
+  Question.prototype.__proto__ = request
+
   this._request = request
   this.user = User
   this.topic = Topic
+  this.question = Question
   this.version = package.version
-
-  User.prototype.__proto__ = request
-  Topic.prototype.__proto__ = request
 }
