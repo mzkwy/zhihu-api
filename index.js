cd3e865282ded16b69b4da151f10cb93dc28055d
syaning <syaningv@gmail.com>
2016-11-14 15:59:32 +0800

    topic api

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 // const answer = require('./lib/api/answer')
 // const org = require('./lib/api/org')
 // const question = require('./lib/api/question')
-// const topic = require('./lib/api/topic')
-const User = require('./lib/api/user')
 const Request = require('./lib/request')
+const User = require('./lib/api/user')
+const Topic = require('./lib/api/topic')
 const package = require('./package')
 
 
@@ -20,7 +20,9 @@ function API(cookie) {
 
   this._request = request
   this.user = User
+  this.topic = Topic
   this.version = package.version
 
   User.prototype.__proto__ = request
+  Topic.prototype.__proto__ = request
 }
